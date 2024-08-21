0a6dfbda7d02fd372ac1db75f5b68557f915425d
edeniarp <zyneticartz@gmail.com>
2024-08-21 19:55:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
-document.getElementById('enter-button').addEventListener('click', function() {
-    document.querySelector('.intro-section').style.display = 'none';
-    document.querySelector('.main-content').style.display = 'block';
+document.addEventListener('DOMContentLoaded', () => {
+    const enterButton = document.getElementById('enter-button');
+    const mainContent = document.querySelector('.main-content');
+    const skillBars = document.querySelectorAll('.skill-bar .progress');
+    const projectItems = document.querySelectorAll('.project-item');
+    const modal = document.getElementById('project-modal');
+    const closeButton = modal.querySelector('.close-button');
+    const modalTitle = modal.querySelector('#project-title');
+    const modalDescription = modal.querySelector('#project-description');
+    const modalImage = modal.querySelector('#project-image');
 
-    setTimeout(function() {
-        document.querySelector('.main-content').style.opacity = '1';
-        animateSkills();
-    }, 10);
-});
+    enterButton.addEventListener('click', () => {
+        document.querySelector('.intro-section').style.display = 'none';
+        mainContent.style.display = 'block';
 
-function animateSkills() {
-    const progressBars = document.querySelectorAll('.progress');
-    progressBars.forEach(bar => {
-        const percentage = bar.getAttribute('data-percentage');
-        bar.style.width = percentage;
+        // Déclencher l'animation de fondu
+        setTimeout(() => {
+            mainContent.style.opacity = '1';
+            // Remplissage des barres de compétences
+            skillBars.forEach(bar => {
+                const percentage = bar.getAttribute('data-percentage');
+                bar.style.width = percentage;
+            });
+        }, 100);
     });
-}
+
+    projectItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const title = item.getAttribute('data-title');
+            const description = item.getAttribute('data-description');
+            const image = item.getAttribute('data-image');
+
+            modalTitle.textContent = title;
+            modalDescription.textContent = description;
+            modalImage.src = image;
+
+            modal.style.display = 'block';
+        });
+    });
+
+    closeButton.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
